ae80388817bf2896e3e62ac9f32184e309a27615
ccy1991911 <ccy1991911@gmail.com>
2023-06-16 16:36:11 -0400

    update

--- condition/code/yitiaolong.sh
+++ condition/code/yitiaolong.sh
@@ -2,3 +2,4 @@ python generate_artificial_sentence.py > ../data/artificial_timer.txt
 cat ../data/24301_para.txt ../data/artificial_timer.txt > ../data/24301_para_n.txt
 python update_condition_result_for_sentences.py
 python print_condition_result.py > ../data/24301_condition.txt
+cp ../data/condition_result_for_sentences.pickle ../../graph/data/
